6ea1e06e88c0e2044dddb99a6525fd01cd95b58c
Vice06 <06jovi@skola.engelholm.se>
2024-09-05 13:40:16 +0200

    add date txt

--- www/public/index.php
+++ www/public/index.php
@@ -1,37 +1,19 @@
-<h1>Webbservern fungerar!</h1>
-<p><strong>Denna sida (index.php) skall bytas ut.</strong></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>WSP-m0</title>
 
-<?php
-    ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-    // Testar att mariaDB fungerar
-     $myPDO = new PDO('mysql:host=mariadb;port=3306;dbname=mysql', 'root', '12345');
-     $result = $myPDO->query("SELECT * FROM db");
-     foreach ($result as $row) {
-        echo $row['Db'] . '<hr>';
-     }
+</head>
+<body>
+   <h1>Webservern fungerar!</h1>
+   <p><strong>denna sida (index.php) ska bytas ut</strong></p>
 
-     phpinfo();
-
-
-      define ('DB_USER', 'root');
-      define ('DB_PASSWORD', '12345'); 
-      define ('DB_HOST', 'mariadb'); // Byt till localhost om inte docker
-      define ('DB_NAME', 'mysql');
-
-      // Byt?
-      $dbConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) or die("Could not connect.");
-
-      // http://php.net/manual/en/function.mysql-set-charset.php
-      mysqli_set_charset($dbConnection,'utf8');
-
-      mysqli_select_db($dbConnection, DB_NAME);
-
-      $sql = "SELECT * FROM db";
-      $rs = mysqli_execute_query($dbConnection, $sql);
-
-      while($row=mysqli_fetch_array($rs)){
-         echo $row['Db'] . '<hr>';
-      }
-?>
+   <?php
+   $date = date("Y-m-d")
+   echo "<h2>PHP dagens datum: $date</h2>";
+   ?>
+   
+</body>
+</html>
