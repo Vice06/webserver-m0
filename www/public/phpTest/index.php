074449061f1b2b5a14450d965c72e1c0aa8cdbea
Vice06 <06jovi@skola.engelholm.se>
2024-09-05 13:06:35 +0200

    init

---
+++ www/public/phpTest/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>PHP - TEST</h1>
+   <?php
+      $version = PHP_VERSION;
+      echo "<h2>Hello PHP " . $version . "<h2>";
+   ?>
+</body>
+</html>
