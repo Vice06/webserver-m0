99056487a589be47d6c386f3f5753dc89339ae08
Vice06 <06jovi@skola.engelholm.se>
2024-09-05 14:20:59 +0200

    uppg 1 klar

--- www/public/uppg1/evaluate.php
+++ www/public/uppg1/evaluate.php
@@ -7,12 +7,22 @@
 <?php
      	$ans1 = $_POST['q1'];
      	$ans2 = $_POST['q2'];
+        $ans3 = $_POST['q3'];
+        $ans4 = $_POST['q4'];
+        $ans5 = $_POST['q5'];
      	$points = 0;
      	
      	if($ans1 == 9)
            $points++;
      	if($ans2 == 15)
            $points++;
+        if($ans3 == 12)
+           $points++;
+        if($ans4 == 3)
+           $points++;
+        if($ans5 == 100)
+           $points++;
+
      	
      	echo("<p>Du fick " . $points . " av 2 möjliga</p>");
 ?>
--- www/public/uppg1/index.php
+++ www/public/uppg1/index.php
@@ -12,6 +12,15 @@
        <label>3 x 5 = </label>
        <input type="text" name="q2">
        <br>
+       <label>6 + 6 = </label>
+       <input type="text" name="q3">
+       <br>
+       <label>1 + 2 = </label>
+       <input type="text" name="q4">
+       <br>
+       <label>10 * 10 = </label>
+       <input type="text" name="q5">
+       <br>
        <input type="submit" value="Rätta">
    </fieldset>
    
